f48ac199142fccd01a7022ec4a501ac7f6f1b5e7
blaettmartin <blaettler.martina@gmail.com>
2021-11-28 17:30:09 +0100

    start Aufbau javascript

--- script/script1.js
+++ script/script1.js
@@ -1,3 +1,12 @@
-function helloworld(){
-    alert('hello world!');
-}
+// selectors
+
+var tableRow = document.getElementsByTagName('tr');
+var tableCell = document.getElementsByTagName('td');
+var tableSlot = document.querySelector('.slot');
+const playerTurn = document.querySelector('.player-turn');
+
+for(let i=0; i < tableCell.length; i++){
+    tableCell[i].addEventListener('click', (e) => {
+        console.log(`${e.target.parentElement.rowIndex},${e.target.cellIndex}`)
+    });
+};
