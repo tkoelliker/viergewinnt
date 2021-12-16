7c9d963a1102b98dd4c7b807035d3c71bef6f0c4
blaettmartin <blaettler.martina@gmail.com>
2021-12-16 07:17:50 +0100

    Kommentare im JS Teil eingefügt

--- connectfour_yt/app.js
+++ connectfour_yt/app.js
@@ -5,55 +5,58 @@ var tableCell = document.getElementsByTagName('td');
 var tableSlot = document.querySelectorAll('.slot');
 const playerTurn = document.querySelector('.player-turn');
 const reset = document.querySelector('.reset');
-
+TODO //Kommentar einfügen
 for(let i=0; i < tableCell.length; i++){
     tableCell[i].addEventListener('click', (e) => {
         console.log(`${e.target.parentElement.rowIndex},${e.target.cellIndex}`)
     });
 };
 
-//namen und farben setzten
+//Namen durch Prompt anfragen setzten, Figur wird durch Reihenfolge gesetzt
 while(!player1){
     var player1 = prompt('Namen Spieler:in 1. Du wirst Spieler 1, der Löwe, sein');
-}
+} 
 player1Color = '#BA671C';
-console.log(player1)
+console.log(player1) // Player 1 gesetzt als Löwe
 
 while(!player2){
     var player2 = prompt('Namen Spieler:in 2. Du wirst Spieler 2, der Elefant, sein');
-}
+} 
 player2Color = '#8BAB23';
-console.log(player2)
+console.log(player2) // Player 2 gesetzt als Elefant
 
+TODO //Kommentar
 player1Name = document.getElementById("player1Avatar").innerHTML= `${player1}`;
 player2Name = document.getElementById("player2Avatar").innerHTML= `${player2}`;
+
 //Status beim Beginn des Spieles
 var currentPlayer =1;
 playerTurn.textContent = `${player1}s Zug!`;
 
-
+//jeder Slot hat zuerst die Farbe Weiss als Hintergrund
 Array.prototype.forEach.call(tableCell, (cell) =>{
     cell.addEventListener('click', changeColor);
     cell.style.backgroundColor = 'white'; //damit der Hintergrund sicher weiss ist der einzelnen Zellen
 
 })
 
-//Spiel
+//eigentliches Spiel, so dass die Farben der einzelnen Slots durch das klicken geändert wird
 function changeColor(e){
     let column = e.target.cellIndex; //Immer wenn man auf eine Zelle klickt, setzt es den Index
     let row = [];
 
-    for (let i = 5; i>-1; i--){ //man startet bei 5 damit die unterste Reihe zuerst geprüft wird
+    for (let i = 5; i>-1; i--){ //man startet bei 5 damit die unterste Reihe zuerst geprüft wird, danach geht es nach "oben"
         if (tableRow[i].children[column].style.backgroundColor =='white'){ //überprüft ob die Hintergrundfarbe weiss ist oder die gleiche wie die eines Spielers
             row.push(tableRow[i].children[column]);
+
             //Schleife für Spieler 1
             if(currentPlayer === 1) {
                 row[0].style.backgroundColor = player1Color; //wenn es Spieler:in 1 ist wird der erste Index des Array in die Spielerfarbe gewechselt und danach zu Spieler 2 gewechslet
-                if (horizontalCheck()|| verticalCheck()|| diagonalCheckUp()||diagonalCheckDown()){ //wenn einer der Checks true wird winner ausgegeben (horizontal or vertical or diagonal)
+                if (horizontalCheck()|| verticalCheck()|| diagonalCheckUp()||diagonalCheckDown()){ //wenn einer der Checks (horizontal or vertical or diagonal) erfüllt ist und true zurück kommt wird winner ausgegeben 
                     playerTurn.textContent=`${player1} hat gewonnen!`;
-                    playerTurn.style.color=player1Color;
+                    playerTurn.style.color=player1Color; // die Farbe von Spieler 1 wird übernommen, da gewonnen
                     return(alert(`${player1} hat gewonnen!!`));
-                }else if(drawCheck()){
+                }else if(drawCheck()){ //wenn kein Feld mehr weiss ist, aber weder horizontal, vertical oder diagonal Checks true ist muss es ein Unentschieden sein. Ausgabe dass es Unentschieden ist
                     playerTurn.textContent ='Es endet unentschieden!';
                     return alert('Unentschieden');
                 }
@@ -80,12 +83,13 @@ function changeColor(e){
     }
 }
 
-//Funktion zur Gewinnprüfung (Feld 1 - 4 müssen die selbe Farbe haben, aber nicht weiss)
+//Funktion zur Gewinnprüfung (Feld 1 - 4 müssen dieselbe Farbe haben, aber nicht weiss). Diese Funktion wird bei den einzelnen Richtungen jeweils übernommen
 function colorMatchCheck (one, two, three, four){
     return(one == two && one === three && one === four && one !== 'white');
 }
 
 //horizontaler Check
+TODO //Kommentar
 function horizontalCheck(){
     for(let row = 0; row < tableRow.length; row++){
         for (let col = 0; col < 4; col++){
@@ -103,7 +107,7 @@ function horizontalCheck(){
 //vertikaler Check
 function verticalCheck(){
         for (let col = 0; col < 7; col++){
-            for(let row=0; row<3; row++){// da man in jeder Reihe drei Mal gewinnen könnte vertikal
+            for(let row=0; row<3; row++){// da man in jeder Reihe drei Mal gewinnen könnte vertikal ist es <3
                 if(colorMatchCheck (tableRow[row].children[col].style.backgroundColor,
                 tableRow[row+1].children[col].style.backgroundColor, 
                 tableRow[row+2].children[col].style.backgroundColor, 
@@ -118,7 +122,7 @@ function verticalCheck(){
 }
 //diagonaler Check aufwärts
 function diagonalCheckUp(){
-    for(let col = 0; col < 4; col++){
+    for(let col = 0; col < 4; col++){ //Da es Diagonal vier Mal Platz hätte: col <4
         for (row = 0; row<3; row++){
             if(colorMatchCheck(tableRow[row].children[col].style.backgroundColor,
                 tableRow[row+1].children[col+1].style.backgroundColor,
@@ -163,6 +167,6 @@ reset.addEventListener('click', ()=>{
         slot.style.backgroundColor = 'white';
     });
     playerTurn.style.Color='black';
-    return(currentPlayer ===1 ? playerTurn.textContent = `${player1}s Zug`: playerTurn.textContent = `${player2}s Zug`);
+    return(currentPlayer ===1 ? playerTurn.textContent = `${player1}s Zug`: playerTurn.textContent = `${player2}s Zug`); // beginn wieder von vorne
 })
 
